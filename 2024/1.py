0cebd2b9f1f38ed40ac6a71327c7f6ef80a2e3af
Ishmeet Singh Saggu <57167867+Ivalocs@users.noreply.github.com>
2024-12-08 00:41:10 +0530

    Update 1.py

--- 2024/1.py
+++ 2024/1.py
@@ -8,9 +8,29 @@ for line in input.readlines():
 
 A.sort()
 B.sort()
+
+# Part 1
 ans = 0
 for i in range(len(A)):
     ans += abs(A[i] - B[i])
-
 print(ans)
+
+# Part 2
+i = 0
+j = 0
+ans_2 = 0
+while (i < len(B)):
+    counter_1 = 1
+    counter_2 = 0
+    while (i+1 < len(A) and A[i] == A[i+1]):
+        counter += 1
+        i += 1
+    while (j < len(B) and B[j] <= A[i]):
+        if (B[j] == A[i]):
+            counter_2 += 1
+        j += 1
+    ans_2 += A[i] * counter_1 * counter_2
+    i += 1
+
+print(ans_2)
 input.close()
