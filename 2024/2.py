3bac45c2e02a49175a6339f982cb00859da9431a
Ishmeet Singh Saggu <57167867+Ivalocs@users.noreply.github.com>
2024-12-13 20:19:23 +0530

    Update 2.py

--- 2024/2.py
+++ 2024/2.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def Check(a, b, c):
     if (c == True):
         return (b > a) and (b - a >= 1) and (b - a <= 3)
@@ -5,10 +7,11 @@ def Check(a, b, c):
         return (a > b) and (a - b >= 1) and (a - b <= 3)
 
 
-input = open('input.txt', 'r')
+input = open('input.txt', 'r').read().strip()
+# Part 1
 safe_reports = 0
-for line in input.readlines():
-    numbers = [int(x) for x in line.rstrip().split(' ') if x != '']
+for line in input.split('\n'):
+    numbers = [int(x) for x in line.rstrip().split(' ')]
     is_increasing = True
     is_safe = True
     for i in range(1, len(numbers)):
@@ -21,6 +24,34 @@ for line in input.readlines():
     if (is_safe):
         safe_reports += 1
 
-
 print(safe_reports)
-input.close()
+
+
+def O_N_N_Code(numbers_origin):
+    for i in range(0, len(numbers_origin)):
+        numbers = deepcopy(numbers_origin)
+        numbers.pop(i)
+        is_increasing = True
+        is_safe = True
+        for i in range(1, len(numbers)):
+            if (i == 1):
+                if (numbers[i] == numbers[i-1]):
+                    is_safe = False
+                if (numbers[i] < numbers[i-1]):
+                    is_increasing = False
+            is_safe = is_safe and Check(numbers[i-1], numbers[i], is_increasing)
+        if (is_safe):
+            return True
+    return False
+
+
+ans = 0
+for line in input.split('\n'):
+    numbers = [int(x) for x in line.rstrip().split(' ')]
+    if (O_N_N_Code(numbers) != O_N_Code(numbers)):
+        print(O_N_N_Code(numbers))
+        print(O_N_Code(numbers))
+        print(numbers)
+        ans += 1
+print(ans)
+    
